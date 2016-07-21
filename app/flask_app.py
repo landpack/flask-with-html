57fc8dee713ed0a1d4e4aa613d16f979934b07e0
landpack <ada.frank.ak@gmail.com>
2016-07-21 20:40:55 +0800

    test iframe tag

--- app/flask_app.py
+++ app/flask_app.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-	return "Hello"
+	return render_template("index.html")
 
+@app.route("/hello")
+def hello():
+	return render_template("hello.html")
 
 
 if __name__ == '__main__':
